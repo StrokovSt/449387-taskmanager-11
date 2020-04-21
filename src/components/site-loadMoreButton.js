319b874c88381359f0865588e2730b16dc93b805
Ivan <strokovhii@gmail.comgit config --global user.name Ivangit config --global user.email strokovhii@gmail.com>
2020-04-21 18:25:13 +0700

    Задание №2. Ничего не понимаю

---
+++ src/components/site-loadMoreButton.js
@@ -0,0 +1,7 @@
+const createSiteLoadMoreButtonTemplate = () => {
+  return (
+    `<button class="load-more" type="button">load more</button>`
+  );
+};
+
+export {createSiteLoadMoreButtonTemplate};
